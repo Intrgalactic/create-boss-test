d4bda3f42b9577aabb8ad45d5b3522de7efa3ea6
Intrgalactic <94009331+Intrgalactic@users.noreply.github.com>
2023-07-28 14:51:49 +0200

    fixed nav font size

--- src/layouts/nav.jsx
+++ src/layouts/nav.jsx
@@ -17,55 +17,63 @@ const NavWithRef = forwardRef((props, ref) => {
   useEffect(() => {
     if (path === "/onboard" && auth.currentUser) {
       fetch(`${import.meta.env.VITE_SERVER_FETCH_URL}get-user?email=${auth.currentUser.email}`).then(res => res.json()).then(data => {
-        setIsPaying(data.isPaying)});
+        setIsPaying(data.isPaying)
+      });
     }
- 
-  }, [setIsPaying,auth.currentUser]);
+
+  }, [setIsPaying, auth.currentUser]);
   return (
     <nav ref={ref}>
       {props.isNavOpened ? <NavButton toggleNav={props.toggleNav} /> : null}
-      {path !=="/onboard" || isPaying == "false" ? <Link to='/'>Home</Link> : null}
-      {path !== "/onboard" ?
+      {path !== "/onboard" || isPaying == "false" ? <Link to='/'>Home</Link> : null}
+      {path.includes('dashboard') ?
         <>
-          <Link>Try It Out</Link>
-          <Link to='/about-us'>About Us</Link>
-          <Link to='/faq'>FAQ</Link>
-          {path === "/" ?
-            <>
-              <Link onClick={(e) => { scrollToItem(e, ".pricing__box") }}>Pricing</Link>
-              <Link onClick={(e) => { scrollToItem(e, ".contact-section__form-container") }}>Contact</Link>
-            </> : null
+          <Link to='/dashboard'>
+            Dashboard
+          </Link>
+          <Link to='/dashboard/services/text-to-speech'>
+            Text To Speech
+          </Link>
+          <Link to='/dashboard/services/speech-to-text'>
+            Speech To Text
+          </Link>
+          <Link to='/dashboard/services/video-section'>
+            Video Section
+          </Link>
+          <Link to='/account-settings'>
+            Account Settings
+          </Link>
+          <Link onClick={logOutUser}>
+            Logout
+          </Link>
+        </> :
+        path !== "/onboard" ?
+          <>
+            <Link>Try It Out</Link>
+            <Link to='/about-us'>About Us</Link>
+            <Link to='/faq'>FAQ</Link>
+            {path === "/" ?
+              <>
+                <Link onClick={(e) => { scrollToItem(e, ".pricing__box") }}>Pricing</Link>
+                <Link onClick={(e) => { scrollToItem(e, ".contact-section__form-container") }}>Contact</Link>
+              </> : null
 
-          }
-          {isLogged ? <Link to='/dashboard'>Dashboard</Link> : <Link to='/sign-in'>Login</Link>}
-          {!isLogged ? <Link to='/sign-up'>Sign Up</Link> : <Link onClick={logOutUser}>Logout</Link>}
+            }
+            {isLogged ? <Link to='/dashboard'>Dashboard</Link> : <Link to='/sign-in'>Login</Link>}
+            {!isLogged ? <Link to='/sign-up'>Sign Up</Link> : <Link onClick={logOutUser}>Logout</Link>}
 
-        </> : <>
-          {isPaying == "false" ?
-            <>
-              <Link to='/account-settings'>
-                Account Settings
-              </Link>
-              <Link onClick={logOutUser}>
-                Logout
-              </Link>
-            </> : 
-            <>
-              <Link to='/dashboard'>
-                Dashboard
-              </Link>
-              <Link to='/dashboard/services/text-to-speech'>
-                Text To Speech
-              </Link>
-              <Link to='/dashboard/services/speech-to-text'>
-                Speech To Text
-              </Link>
-              <Link to='/dashboard/services/video-section'>
-                Video Section
-              </Link>
-            </>
-          }
-        </>
+          </> : <>
+            {isPaying == "false" &&
+              <>
+                <Link to='/account-settings'>
+                  Account Settings
+                </Link>
+                <Link onClick={logOutUser}>
+                  Logout
+                </Link>
+              </>
+            }
+          </>
       }
 
     </nav>
