30c2f897f8ffa13b875058ab3651f4474a55fc9d
Intrgalactic <94009331+Intrgalactic@users.noreply.github.com>
2023-08-05 23:56:34 +0200

    Update speechToText.js

--- middleware/api/speechToText.js
+++ middleware/api/speechToText.js
@@ -2,30 +2,14 @@ const asyncHandler = require("express-async-handler")
 const speech = require('@google-cloud/speech')
 const setContentType = require('../utils/setContentType');
 const sendToStorage = require('./sendToStorage');
-const {getAudioDurationInSeconds} = require('get-audio-duration');
-const which = require('which');
-const MemoryStreams = require('memory-streams');
 
 const speechToText = (storage) => {
     return asyncHandler(async (req, res) => {
-        const ffprobePath = await new Promise((resolve, reject) => {
-            which('ffprobe', (err, path) => {
-              if (err) {
-                reject(err);
-              } else {
-                resolve(path);
-              }
-            });
-          });
-      
         const client = new speech.SpeechClient();
         const encoding = 'WEBM_OPUS';
         const sampleRateHertz = 16000;
         const languageCode = req.body.code;
         const outputFileName = req.file.originalname;
-        const readableStream = new MemoryStreams.ReadableStream(req.file.buffer);
-        const secs = await getAudioDurationInSeconds(req.file.buffer,{ffprobePath});
-        console.log(secs);
         const contentType = setContentType(req.body.audioEncoding);    
         const config = {
             encoding: encoding,
