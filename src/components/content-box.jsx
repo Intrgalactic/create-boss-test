e78ad9f2c188ce33f66a510b7793943da8b0d88a
Intrgalactic <94009331+Intrgalactic@users.noreply.github.com>
2023-08-22 17:19:24 +0200

    added link hover effects

--- src/components/content-box.jsx
+++ src/components/content-box.jsx
@@ -1,13 +1,12 @@
 import { CtaButton } from "./cta-button";
-import { motion } from "framer-motion";
 
 export function ContentBox({ heading, description, children,boxClass,buttonText }) {
     return (
-        <motion.div className={`content-box ${boxClass ? boxClass : ''}`} whileHover={{scale:1.2}} transition={{duration:0.1}}>
+        <div className={`content-box ${boxClass ? boxClass : ''}`} >
             {children}
             <h3>{heading}</h3>
             <p>{description}</p>
             {buttonText ? <CtaButton text={buttonText}/> : null}
-        </motion.div>
+        </div>
     )
 }
